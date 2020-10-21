ad84d4d7657a6679135c9826bff64fd03d897f78
Dusan Grbic <dusangrbic984@gmail.com>
2020-10-21 09:18:10 +0200

    Page Scroll Progress Bar js simplified by using jQuery for smooth scroll animation

--- js/page-scroll-progress-bar.js
+++ js/page-scroll-progress-bar.js
@@ -1,58 +1,24 @@
-const pbc = document.querySelector('.progressbar-container')
-const pb = document.querySelector('.progressbar')
-const scrollDuration = 200
-/* dh - document height; wh - window height; sh - scroll height (total height - viewport) */
-let dh, wh, sh
+$(document).ready(function () {
 
-// On load and resize determine the heights
-['load', 'resize'].forEach(e => window.addEventListener(e, e => {
-   dh = document.body.clientHeight
-   wh = window.innerHeight
-   sh = dh - wh
-}))
+   const pbc = document.querySelector('.progressbar-container')
+   const pb = document.querySelector('.progressbar')
+   const duration = 200
+   /* dh - document height; wh - window height; sh - scroll height (total height - viewport) */
+   let dh, wh, sh
 
-// On scroll adjust progressbar height
-window.onscroll = e => pb.style.height = 100 / sh * pageYOffset + '%'
+   // On load and resize determine the heights
+   ['load', 'resize'].forEach(e => window.addEventListener(e, e => {
+      dh = document.body.clientHeight
+      wh = window.innerHeight
+      sh = dh - wh
+   }))
 
-// Smooth scroll the page (progressbar height is automatically adjusted because of window.onscroll listener)
-pbc.onclick = e => {
-   const scrollPercent = 100 / wh * e.offsetY
-   const scroll = sh / 100 * scrollPercent
-   smoothScroll(scroll, scrollDuration)
-}
+   // On scroll adjust progressbar height
+   window.onscroll = e => pb.style.height = 100 / sh * pageYOffset + '%'
 
-function smoothScroll(target, duration) {
-   let t0 = null
-   // starting scroll point
-   const startPosition = pageYOffset
-   // difference between ending and starting scroll point
-   const distance = target - startPosition
+   // Smooth scroll the page (progressbar height is automatically adjusted because of window.onscroll listener)
+   pbc.onclick = e => $('html, body').animate({
+      scrollTop: e.offsetY / wh * sh
+   }, duration)
 
-   function animation(t) {
-      // set t0 to timestamp value when animation started
-      if (t0 === null) t0 = t
-
-      // Time elapsed since the begining of the animation
-      const timeElapsed = t - t0
-      const run = ease(timeElapsed, startPosition, distance, duration)
-
-      scrollTo(0, run)
-      if (timeElapsed < duration) requestAnimationFrame(animation)
-   }
-   requestAnimationFrame(animation)
-
-
-   function ease(t, b, c, d) {
-      /**
-       * t - current time
-       * b - start value
-       * c - change in value
-       * d - duration
-       */
-      t /= d / 2;
-      if (t < 1) return c / 2 * t * t + b;
-      t--;
-      return -c / 2 * (t * (t - 2) - 1) + b;
-   };
-}
-
+})
